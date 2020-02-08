bd35297b0c54570b07aeab9c3edbe0cc9c624d2f
brye <bryewalks@gmail.com>
2020-02-08 17:37:38 -0600

    initial commit

---
+++ lib/crypto_checker.rb
@@ -0,0 +1,6 @@
+require "crypto_checker/version"
+
+module CryptoChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/crypto_checker/version.rb
@@ -0,0 +1,3 @@
+module CryptoChecker
+  VERSION = "0.1.0"
+end
