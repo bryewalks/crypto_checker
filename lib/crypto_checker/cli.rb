09fba7a3e3c6038254246de683ab7edc25d4335b
brye <bryewalks@gmail.com>
2020-02-09 22:16:46 -0600

    working cli

--- lib/crypto_checker/cli.rb
+++ lib/crypto_checker/cli.rb
@@ -1,23 +1,35 @@
 require "thor"
+require "crypto_checker/crypto_api"
 
 module CryptoChecker
   class CLI < Thor
 
     DEFAULT_CURRENCY = "usd"
 
-    desc "price COIN CURRENCY", "checks price of cryptocurrence vs specified currency. Default currency of USD"
+    desc "price [COIN CURRENCY]", "Checks price of cryptocurrency vs specified currency. Default currency of USD."
     def price(coin, currency = DEFAULT_CURRENCY)
-      output = ""
-      output += CryptoAPI.check_price(coin, currency).to_s
+      price = CryptoAPI.check_price(coin, currency).to_s
       if currency == "usd"
-        output.insert(0, "$")
+        price.insert(0, "$")
       else
-        output.insert(-1, " #{currency}")
+        price.insert(-1, " #{currency.upcase}")
       end
-      puts output
+      puts price
     end
 
-    desc "supported", "Lists supported currencys to check crypto against."
+    desc "cost [AMOUNT COIN CURRENCY]", "Finds the cost of specified amount of cryptocurrency. Default currency of USD."
+    def cost(amount, coin, currency = DEFAULT_CURRENCY)
+      cost = CryptoAPI.calculate_cost(amount.to_i, coin, currency).to_s
+      if currency == "usd"
+        cost = '%.2f' % cost
+        cost.insert(0, "$")
+      else
+        cost.insert(-1, " #{currency.upcase}")
+      end
+      puts cost
+    end
+
+    desc "supported", "Lists supported currencies to check cryptocurrency against."
     def supported
       results = CryptoAPI.supported_vs_currencies
       results.each do |currency|
